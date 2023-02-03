c04f7bf003b514a1cd97e6e7bcffaf613ce74e2a
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-02-03 23:09:28 +0545

    Socket Network using AES and RSA

--- socketNetwork/socketClient.py
+++ socketNetwork/socketClient.py
@@ -1,19 +1,40 @@
 import socket
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+def generate_keys():
+    private_key = RSA.generate(1024)
+    public_key = private_key.public_key()
+    return private_key,public_key
 
 def client_program():
     host = socket.gethostname()  
     port = 9500  # socket server port number
-
+    private_key, public_key = generate_keys()
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ") 
+    client_socket.send(public_key.export_key())
+    encryption_session_key = client_socket.recv(1024)
+    rsa_decrypt = PKCS1_OAEP.new(private_key)
+    session_key = rsa_decrypt.decrypt(encryption_session_key)
+    cipher_aes = AES.new(session_key, AES.MODE_CBC, session_key)
+    message = bytes(input("Text from Client -> "),'utf-8') 
 
     while message.lower().strip() != 'end':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-        print('Received from server: ' + data)  
-        message = input("Enter the message-> ")  # again take input
+        pad = AES.block_size - (len(message)%AES.block_size)
+        message += bytes([pad]) * pad
+        cipher_aes = AES.new(session_key, AES.MODE_CBC, session_key)
+        ciphertext= cipher_aes.encrypt(message)
+        print("Encrypted Message:", ciphertext)
+        client_socket.send(ciphertext)  # send message
+        data = client_socket.recv(1024)  
+        if not data:
+            break
+        cipher_aes = cipher_aes = AES.new(session_key, AES.MODE_CBC, session_key)
+        plain_response = cipher_aes.decrypt(data)
+        plain_response = plain_response[:- plain_response[-1]]
+        print('Message Received from server: ' + str(plain_response))  
+        message = bytes(input("Enter the message-> "), "utf-8")  # again take input
 
     client_socket.close()  # close the connection
 
--- socketNetwork/socketServer.py
+++ socketNetwork/socketServer.py
@@ -1,25 +1,48 @@
 import socket
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import AES, PKCS1_OAEP
 
+def generate_keys():
+    private_key = RSA.generate(1024)
+    public_key = private_key.publickey()
+    
+    return private_key, public_key
 
 def server_program():
-    # get the hostname
-    host = socket.gethostname()
+    session_key = Random.new().read(AES.block_size)
+    host = socket.gethostname() 
     port = 9500  #  port no. above 1024
 
-    server_socket = socket.socket()  # get instance
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # get instance
     server_socket.bind((host, port))  #bind host and port
 
     server_socket.listen(2) #configure no. of clients the server can listen to(simultaneously)
     conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    
+    print("Connection from Client: " + str(address))
+    client_public_key = RSA.import_key(conn.recv(1024))
+    cipher_AES = AES.new(session_key,AES.MODE_CBC,session_key)
+
+    rsa_encrypt = PKCS1_OAEP.new(client_public_key)
+    encrypt_session_key = rsa_encrypt.encrypt(session_key)
+    conn.send(bytes(encrypt_session_key))
     while True:
-        data = conn.recv(1024).decode() #receive the data packet less than 1024 bytes
+        data = conn.recv(1024) #receive the data packet less than 1024 bytes
         if not data:
             break
-        print("Sent from connected user: " + str(data))
-        data = input('Enter the message-> ')
-        conn.send(data.encode())  # send data to the client
+        plain_text = cipher_AES.decrypt(data)
+        plain_text = plain_text[:-plain_text[-1]]
+        print("Sent from connected user: " + str(plain_text))
+        message = bytes(input('Enter the response message-> '),"utf-8")
+        
+        if message.lower().strip() ==b'end':
+            break
+    
+        pad= AES.block_size - (len(message) % AES.block_size)
+        message += bytes([pad]) * pad
+        cipher_AES = AES.new(session_key,AES.MODE_CBC,session_key)
+        ciphertext = cipher_AES.encrypt(message)
+        conn.send(ciphertext)  # send data to the client
 
     conn.close()  # close the connection
 
